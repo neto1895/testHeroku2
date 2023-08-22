84381f26a3f1e27ddec5e1eda9c6f49468711466
neto1895 <cruz.rosales.ernesto@gmail.com>
2023-08-21 18:38:15 -0600

    deploy5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+import { defineConfig } from "../node_modules/vite/dist/node/index.js";
+//"file:///C:/Users/HOME/Documents/Universidad/Coding%20Bootcamp/Projects/Project-3/Cre8tive/client/node_modules/vite/dist/node/index.js";
+import react from "../node_modules/@vitejs/plugin-react/dist/index.mjs";
+
+//"file:///C:/Users/HOME/Documents/Universidad/Coding%20Bootcamp/Projects/Project-3/Cre8tive/client/node_modules/@vitejs/plugin-react/dist/index.mjs";
+
 import { webfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
